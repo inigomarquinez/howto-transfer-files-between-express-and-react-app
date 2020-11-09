ef66bb91e1937e1d1ede80cfd29e12bf01c00c09
Íñigo Marquínez <inigo.marquinez@guidesmiths.com>
2020-11-09 17:23:30 +0100

    chore: clean server code

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-console */
 require('dotenv').config();
-const { join } = require('path');
 const { exit } = require('process');
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const { join } = require('path');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 const colors = require('colors');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const multer = require('multer');
 
+// For DiskStorage
 const upload = multer({ dest: 'uploads/' });
+// For MemoryStorage
 // const storage = multer.memoryStorage();
 // const upload = multer({ storage });
 
@@ -26,47 +28,31 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/helloWorld', (_req, res) => {
-  res.json('Hello World!');
-});
+app.get(
+  '/helloWorld',
+  (_req, res) => {
+    res.json('Hello World!');
+  },
+);
 
-app.get('/download', (_req, res) => {
-  // res.download(
-  //   join(__dirname, 'assets', 'file-to-download.zip'),
-  //   'file-to-download.zip',
-  //   (error) => {
-  //     if (error) {
-  //       console.error('Error downloading file: >>>', error);
-  //     } else {
-  //       console.info('File downloaded!');
-  //     }
-  //   },
-  // );
-  res.download(join(__dirname, 'assets', 'file-to-download.zip'));
-});
+app.get(
+  '/download',
+  (_req, res) => {
+    res.download(join(__dirname, 'assets', 'file-to-download.zip'));
+  },
+);
 
-app.get('/long', async (_req, res) => {
-  const writeToRes = (counter) => {
-    setTimeout(() => {
-      if (counter > 0) {
-        res.write(`${JSON.stringify({ counter })}`);
-        writeToRes(counter - 1);
-      } else {
-        res.end();
-      }
-    }, 1000);
-  };
+app.post(
+  '/upload',
+  upload.single('file'),
+  (req, res) => {
+    const { file } = req;
+    console.log('file :>> ', file);
 
-  writeToRes(10);
-});
-
-app.post('/upload', upload.single('file'), (req, res) => {
-  const { file } = req;
-  console.log('file :>> ', file);
-
-  res.send('File uploaded!');
-});
+    res.send('File uploaded!');
+  },
+);
 
 app.listen(process.env.PORT, () => {
-  console.log(colors.green(`Server listening at http://localhost:${process.env.PORT}`));
+  console.log(colors.green(`Server listening at port ${process.env.PORT}`));
 });
